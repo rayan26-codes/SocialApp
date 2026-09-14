a0f7d1098acee4b7c8d07ca770f51e728f8ba78d
Rayan <rayan26ry@gmail.com>
2026-09-14 16:39:43 +0500

    changed login text

--- Views/Login.php
+++ Views/Login.php
@@ -12,7 +12,7 @@
     <main class="auth-container">
         <section class="auth-card">
             <header class="auth-header">
-                <h2>Welcome Back user</h2>
+                <h2>Welcome Back again User</h2>
                 <p>Please enter your details to sign in</p>
             </header>
 
