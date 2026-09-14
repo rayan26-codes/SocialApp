2d911feb5f995c58a9eada7e675f810c66386fe0
Rayan <rayan26ry@gmail.com>
2026-09-14 16:51:52 +0500

    added new login text

--- Views/Login.php
+++ Views/Login.php
@@ -12,7 +12,7 @@
     <main class="auth-container">
         <section class="auth-card">
             <header class="auth-header">
-                <h2>Welcome Back User</h2>
+                <h2>Welcome Back User, whats on your mind?</h2>
                 <p>Please enter your details to sign in</p>
             </header>
 
